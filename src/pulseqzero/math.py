10adaafd479b4cd3f648947d05ba1c512b7b7317
J-Endres <jonathan.endres01@gmail.com>
2024-12-06 14:45:27 +0100

    fixed pp rounding to support non-tensors

--- src/pulseqzero/math.py
+++ src/pulseqzero/math.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 class Ceil(torch.autograd.Function):
@@ -43,19 +44,28 @@ class Round(torch.autograd.Function):
         return grad_output
 
 
-def ceil(x: torch.Tensor) -> torch.Tensor:
+def ceil(x: torch.Tensor | float) -> torch.Tensor:
     """Differentiable version of torch.ceil.
     For gradient calculation, this mimicks the identity function."""
-    return Ceil.apply(x)
+    try:
+        return Ceil.apply(x)
+    except TypeError:
+        return torch.as_tensor(np.ceil(x))
 
 
 def floor(x: torch.Tensor) -> torch.Tensor:
     """Differentiable version of torch.floor.
     For gradient calculation, this mimicks the identity function."""
-    return Floor.apply(x)
+    try:
+        return Floor.apply(x)
+    except TypeError:
+        return torch.as_tensor(np.floor(x))
 
 
 def round(x: torch.Tensor) -> torch.Tensor:
     """Differentiable version of torch.round.
     For gradient calculation, this mimicks the identity function."""
-    return Round.apply(x)
+    try:
+        return Round.apply(x)
+    except TypeError:
+        return torch.as_tensor(np.round(x))
