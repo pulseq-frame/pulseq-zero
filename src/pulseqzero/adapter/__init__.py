1047481ad6f9a085f810c2ce8fe5e20cf1b4085f
J-Endres <jonathan.endres01@gmail.com>
2025-07-24 11:49:42 +0200

    make calc_duration differentiable

--- src/pulseqzero/adapter/__init__.py
+++ src/pulseqzero/adapter/__init__.py
@@ -7,11 +7,12 @@ def make_label(label, type, value):
 
 
 def calc_duration(*args):
-    # We assume that all events have a duration property
-    return max(
-        (event.duration for event in args if event is not None),
-        default=0.0
-    )
+    import torch  # needed for differentiability
+    duration = torch.zeros(1)
+    for event in args:
+        if event is not None:
+            duration = torch.maximum(duration, torch.as_tensor(event.duration))
+    return duration
 
 
 def calc_rf_bandwidth(rf, cutoff=0.5, return_axis=False, return_spectrum=False):
