7a58618e647966fe4e7c4276d6c7bd3eaecba3bd
felixdietz <felix.dietz@fau.de>
2026-01-08 14:46:49 +0100

    Support FreeGrad simultaneously to RF

--- src/pulseqzero/adapter/seq_convert.py
+++ src/pulseqzero/adapter/seq_convert.py
@@ -186,14 +186,27 @@ def parse_pulse(delay, rf, grad_x, grad_y, grad_z, samples: int) -> list[TmpPuls
         events.append(calc_spoiler(t_grad[i], t_grad[i + 1]))
         flip, phase = integrate_pulse(rf, t_rf[i], t_rf[i + 1])
         
-        if grad_x: grad_ampl_x = grad_x.amplitude
-        else: grad_ampl_x = 0
-        
-        if grad_y: grad_ampl_y = grad_y.amplitude
-        else: grad_ampl_y = 0
-        
-        if grad_z: grad_ampl_z = grad_z.amplitude
-        else: grad_ampl_z = 0            
+        if grad_x: 
+            # distinguish between TrapGrad and FreeGrad
+            if isinstance(grad_x, FreeGrad):
+                # assuming pulse center and gradient waveform are aligned
+                grad_ampl_x = grad_x.waveform[len(grad_x.waveform)//2]
+            else: grad_ampl_x = grad_x.amplitude 
+        else: 
+            grad_ampl_x = 0
+            
+        if grad_y: 
+            if isinstance(grad_y, FreeGrad): grad_ampl_y = grad_y.waveform[len(grad_y.waveform)//2]  
+            else: grad_ampl_y = grad_y.amplitude 
+        else:             
+            grad_ampl_y = 0 
+            
+        if grad_z: 
+            if isinstance(grad_z, FreeGrad):
+                grad_ampl_z = grad_z.waveform[len(grad_z.waveform)//2] 
+            else: grad_ampl_z = grad_z.amplitude 
+        else: 
+            grad_ampl_z = 0
         
         events.append(TmpPulse(flip, phase, step, rf.freq_offset, grad_ampl_x, grad_ampl_y, grad_ampl_z, rf.shim_array, use))
     events.append(calc_spoiler(t_grad[-2], t_grad[-1]))
